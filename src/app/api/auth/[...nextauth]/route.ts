4f8fb99ec638edb0956c5b8f40516a50defc2b07
Davitmach <wvime30@gmail.com>
2025-01-07 17:24:40 +0400

    d

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,16 @@
-// import NextAuth from "next-auth"
-// import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
 
-// const handler = NextAuth({
-//   providers: [
-//     GoogleProvider({
-//      clientId:process.env.NEXT_GOOGLE_CLIENTID || '',
-//      clientSecret:process.env.NEXT_GOOGLE_SECRET || ''
-//     })
-     
-//    ],
-// })
-// export { handler as GET, handler as POST };
-
-
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-export default NextAuth({
+const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.NEXT_GOOGLE_CLIENTID||'',
-      clientSecret: process.env.NEXT_GOOGLE_SECRET||'',
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET, 
+     clientId:process.env.NEXT_GOOGLE_CLIENTID || '',
+     clientSecret:process.env.NEXT_GOOGLE_SECRET || ''
+    })
+     
+   ],
+   secret:process.env.NEXTAUTH_SECRET
+})
+export { handler as GET, handler as POST };
 
-});
+
