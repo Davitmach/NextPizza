b361d721b94bbb17dbda479b8e283476cd22300b
Davitmach <wvime30@gmail.com>
2025-01-07 17:18:03 +0400

    Auth2.0

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,30 +1,15 @@
-// import NextAuth from "next-auth"
-// import GoogleProvider from "next-auth/providers/google"
-
-// const handler = NextAuth({
-//   providers: [
-//     GoogleProvider({
-//      clientId:process.env.NEXT_GOOGLE_CLIENTID || '',
-//      clientSecret:process.env.NEXT_GOOGLE_SECRET || ''
-//     })
-     
-//    ],
-// })
-// export { handler as GET, handler as POST };
-
-
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
 
-const authOptions = {
-  // Configure one or more authentication providers
+const handler = NextAuth({
   providers: [
-    GithubProvider({
-      clientId: process.env.NEXT_GOOGLE_CLIENTID || '',
-      clientSecret: process.env.NEXT_GOOGLE_SECRET || ''
-    }),
-    // ...add more providers here
-  ],
-}
+    GoogleProvider({
+     clientId:process.env.NEXT_GOOGLE_CLIENTID || '',
+     clientSecret:process.env.NEXT_GOOGLE_SECRET || ''
+    })
+     
+   ],
+})
+export { handler as GET, handler as POST };
 
-export default NextAuth(authOptions)
+
