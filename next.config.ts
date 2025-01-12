757bbc35880be2f1c657b73527d62fd079540aa4
Davitmach <wvime30@gmail.com>
2025-01-12 21:56:50 +0400

    qaq

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['media.dodostatic.com','s3-alpha-sig.figma.com','media.dodostatic.net','cdn.dodostatic.net'],
+    domains: ['cs2.pikabu.ru','media.dodostatic.com','s3-alpha-sig.figma.com','media.dodostatic.net','cdn.dodostatic.net'],
     
   },
 };
