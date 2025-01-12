35b112537079a9febdf06a996e045ec5aff59f70
Davitmach <wvime30@gmail.com>
2025-01-12 16:56:56 +0400

    qaq

--- src/app/(root)/@modal/(.)product/[id]/page.tsx
+++ src/app/(root)/@modal/(.)product/[id]/page.tsx
@@ -1,14 +1,14 @@
+
 import { ModalPage } from '@/components/shared/modal/modal';
 
-type ModalProps = {
-  params: { id: string }; // Dynamic route parameters are usually strings
-};
+export default async function Modal({params}:any) {
 
-export default async function Modal({ params }: ModalProps) {
-  const id = parseInt(params.id, 10); // Convert the `id` to a number if needed
 
-  return <ModalPage id={id} />;
+
+const {id} = await params;
+if(id) {
+    return <ModalPage id={id}/>
+}
 }
 
 
-  
