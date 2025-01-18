11d457fd712670e9bded6956932314924e4afe1b
Davitmach <wvime30@gmail.com>
2025-01-18 20:56:36 +0400

    de

--- src/components/shared/emptyCart/emptyCart.tsx
+++ src/components/shared/emptyCart/emptyCart.tsx
@@ -19,11 +19,11 @@ ref.current?.addEventListener('animationend',()=> {
 
     return(
 
-        <div ref={ref} className={`${Style.cart} flex flex-col justify-center items-center ${disable ==true && Style.disable}`}>
+        <div ref={ref} className={`${Style.cart} px-[10px] flex flex-col justify-center items-center ${disable ==true && Style.disable}`}>
              <div className={`Empty flex flex-col justify-center items-center `}>
                        <Image priority src={'/emptyBox.png'} alt="EmptyBox" width={120} height={120} className="mx-auto"/>
                        <div className="Title text-center"><h1 className="text-black-label font-[600] text-[22px]">Корзина пустая</h1></div>
-                <div className="Description text-center"><p className="font-[400] text-[16px] text-[#a6a6a6] mt-[6px]">Добавьте хотя бы одну пиццу, чтобы совершить заказ</p></div>
+                <div className="mb-[20px] Description text-center"><p className="font-[400] text-[16px] text-[#a6a6a6] mt-[6px]">Добавьте хотя бы одну пиццу, чтобы совершить заказ</p></div>
                 
                    </div>
             <Button func={Back} variant='back' size="default" status={false} />
