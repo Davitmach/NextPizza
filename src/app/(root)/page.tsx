3188fc27c123c96192ff7e3f62443e094494c9fd
Davitmach <wvime30@gmail.com>
2025-01-19 16:13:27 +0400

    de

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import { CategoryBox } from "@/components/shared/category/category";
 import { Notifications } from "@/components/shared/notification/notification";
 import PayPalButton from "@/components/shared/paypal/paypal";
@@ -8,29 +8,36 @@ import { StoriesBox } from "@/components/shared/stories/stories";
 import { BigContainer } from "@/components/UI/container/container";
 import { Select } from "@/components/UI/select/select";
 import { Title } from "@/components/UI/title/title";
-import { useCallback, useEffect, useState } from "react";
-import {Button} from '@/components/UI/button/button'
-import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-const qaq = localStorage.getItem('NEXT_PIZZA_DISABLE_USER_AUTH_TOKEN');
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Это код будет выполняться только на клиенте
+    if (typeof window !== 'undefined') {
+      const storedToken = localStorage.getItem('NEXT_PIZZA_DISABLE_USER_AUTH_TOKEN');
+      setToken(storedToken);
+    }
+  }, []);
 
   return (
     <>
+      <BigContainer className="flex flex-col gap-3 overflow-hidden" parentClassName="shadow-lg"> 
+        <Title>Все пиццы</Title>
+        <div className="w-full flex items-center justify-between xs:flex-col xs:items-start xs:gap-4">
+          <CategoryBox type="server"/>
+          <Select/>
+        </div> 
+      </BigContainer>
 
-<BigContainer className="flex flex-col gap-3  overflow-hidden " parentClassName="shadow-lg"> 
-  <Title>Все пиццы</Title>
-  <div className="w-full flex items-center justify-between xs:flex-col xs:items-start xs:gap-4">
-    <CategoryBox type='server'/>
-    <Select/>
-   </div> 
- </BigContainer>
-{qaq && qaq}
-  <BigContainer>
-  <StoriesBox/>
-</BigContainer>
+      {token && <div>{token}</div>} {/* Выводим токен, если он есть */}
 
+      <BigContainer>
+        <StoriesBox/>
+      </BigContainer>
 
-<Notifications/>
+      <Notifications/>
     </>
   );
 }
