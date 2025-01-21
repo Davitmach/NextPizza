ca3f4e51cbaf883b184ad5e664879ad7801de59b
Davitmach <wvime30@gmail.com>
2025-01-21 18:16:26 +0400

    de

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -2,6 +2,9 @@
 export async function POST(request: any) {
     const data = await request.json();
     const token = data;
+    console.log('Received notification:', data);
+console.log('Sending confirmation response to YooKassa');
+
     return new Response(JSON.stringify({ token: token }), {
         status: 200,
       });
