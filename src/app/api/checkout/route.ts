27eb8fed47bf94570b9cce1aa0d0cfd94c1759f8
Davitmach <wvime30@gmail.com>
2025-01-21 20:56:04 +0400

    de

---
+++ src/app/api/checkout/route.ts
@@ -0,0 +1,19 @@
+export async function POST(request: Request) {
+    try {
+      // Получаем данные из тела запроса
+      const data = await request.json();
+  
+   
+  
+      // Возвращаем ответ с этим токеном
+      return new Response(JSON.stringify({ data }), {
+        status: 200,
+      });
+    } catch (error) {
+      // Обработка ошибок, если данные не могут быть распарсены
+      return new Response(JSON.stringify({ error: 'Invalid request data' }), {
+        status: 400,
+      });
+    }
+  }
+  
