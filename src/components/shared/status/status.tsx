a8e0e522e32e3da225ddbcd271ad19c874aed80d
Davitmach <wvime30@gmail.com>
2025-01-24 18:15:53 +0400

    de

--- src/components/shared/status/status.tsx
+++ src/components/shared/status/status.tsx
@@ -8,19 +8,19 @@ interface IStatus {
 export const Status = (props:IStatus)=> {
     if(props.status == 'SUCCEEDED') {
         return(
-            <div className="cursor-pointer flex items-center justify-center px-[10px] h-[35px] bg-green-bg text-green-text font-[600] text-[14px] rounded-[20px] ">Оплачено</div>
+            <div className="cursor-pointer flex items-center justify-center px-[10px] h-[35px] text-nowrap bg-green-bg text-green-text font-[600] text-[14px] rounded-[20px] ">Оплачено</div>
         )
     }
     else if(props.status == 'PENDING') {
         return(
             <div onClick={()=> {
                 document.location.href= `https://yoomoney.ru/checkout/payments/v2/contract?orderId=${props.id}`
-            }} className="cursor-pointer flex items-center justify-center px-[10px]  h-[35px] bg-yellow-bg text-yellow-text font-[600] text-[14px] rounded-[20px] ">В ожидании</div>
+            }} className="text-nowrap cursor-pointer flex items-center justify-center px-[10px]  h-[35px] bg-yellow-bg text-yellow-text font-[600] text-[14px] rounded-[20px] ">В ожидании</div>
         )
     }
     else {
         return(
-            <div className="cursor-pointer flex items-center justify-center  px-[10px] h-[35px] bg-red-bg text-red-text font-[600] text-[14px] rounded-[20px] ">Отклонено</div>
+            <div className=" text-nowrapcursor-pointer flex items-center justify-center  px-[10px] h-[35px] bg-red-bg text-red-text font-[600] text-[14px] rounded-[20px] ">Отклонено</div>
         ) 
     }
 }
