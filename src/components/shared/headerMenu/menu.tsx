2f24d63b3edc27d6380e6232e46f11e833bab17d
Davitmach <wvime30@gmail.com>
2025-01-27 10:31:31 +0400

    de

--- src/components/shared/headerMenu/menu.tsx
+++ src/components/shared/headerMenu/menu.tsx
@@ -25,7 +25,7 @@ else {
 if(status == true) {
     return (
         <div className="relative">
-        <div ref={ref} className={` active z-50 py-[20px] px-[30px] duration-500 absolute left-0 top-[0px]  w-full   bg-white flex justify-center gap-5 flex-col-reverse shadow-2xl rounded-b-3xl`}>
+        <div ref={ref} className={` active z-50 py-[20px] px-[30px] duration-500 absolute left-0 top-[0px]  w-full   bg-white flex justify-center gap-5 flex-col-reverse shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1),0_2px_4px_-1px_rgba(0,0,0,0.06)] rounded-b-3xl`}>
             { active == false && <div className=" "><Header_input/></div>}
                         <div className="flex gap-[16px] justify-center ">
                             <div><Button variant='user' status={false} size="default"  /></div>
@@ -39,7 +39,7 @@ else {
     if(click == true) {
 return (
     <div className="relative">
-        <div ref={ref} className={`translate-x-full duration-500 disable z-50 py-[20px] px-[30px] absolute left-0   top-[0px]  w-full  bg-white flex justify-center gap-5 flex-col-reverse  shadow-2xl   rounded-b-3xl`}>
+        <div ref={ref} className={`translate-x-full duration-500 disable z-50 py-[20px] px-[30px] absolute left-0   top-[0px]  w-full  bg-white flex justify-center gap-5 flex-col-reverse  shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1),0_2px_4px_-1px_rgba(0,0,0,0.06)]   rounded-b-3xl`}>
              { active == false && <div className=" "><Header_input/></div>}
                         <div className="flex gap-[16px] justify-center flex-1 ">
                             <div><Button variant='user' status={false} size="default"  /></div>
@@ -52,7 +52,7 @@ return (
     else {
         return (
             <div className="relative">
-        <div ref={ref} className={` z-50 py-[20px] disable px-[30px] absolute left-0 translate-x-full  top-[0px]  w-full  bg-white flex justify-center gap-5 flex-col-reverse  shadow-2xl rounded-b-3xl `}>
+        <div ref={ref} className={` z-50 py-[20px] disable px-[30px] absolute left-0 translate-x-full  top-[0px]  w-full  bg-white flex justify-center gap-5 flex-col-reverse  shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1),0_2px_4px_-1px_rgba(0,0,0,0.06)] rounded-b-3xl `}>
                { active == false && <div className=" "><Header_input/></div>}
                         <div className="flex gap-[16px] justify-center flex-1 ">
                             <div><Button variant='user' status={false} size="default"  /></div>
